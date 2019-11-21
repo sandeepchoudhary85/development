574f4dc9fd8c8080f27f6c3ec7417f3521c19c33
sandeepchoudhary85 <sandeepchoudhary85@gmail.com>
2019-11-21 13:57:23 +0530

    Create Custom Menu html structure
     Create Custom Menu html structure

--- wordpress-help.php
+++ wordpress-help.php
@@ -61,3 +61,84 @@ function create_our_work() {
      );
 }
 /**********create Custom post with custom category********/
+
+
+/************ Create Custom Menu html structure ***********/
+
+function topheadermenu(){
+
+	$menu_name = 'menu-1';
+	if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+	    $menu = wp_get_nav_menu_object( $locations[ $menu_name ] );
+	  
+	   
+
+	   $menuitems = wp_get_nav_menu_items($menu->term_id);
+	   //echo "<pre>";print_r($menu_items);
+	   $count = 0;
+	  $submenu = false;
+	  //echo "<pre>"; print_r($menuitems['1']);
+
+	  echo '<ul class="nav navbar-nav">';
+	   foreach( $menuitems as $item ) { 
+
+	   			  if($menuitems[$count + 1]->menu_item_parent == $item->ID)
+		   			  {
+		   			  		$dropdownclass= 'dropdown';
+		   			  		$icon = '<b class="caret"></b>';
+		   			  		$hrefclass = 'dropdown-toggle';	
+		   			  }
+		   			else{ 
+			   				$dropdownclass= ''; 
+			   				$icon = ''; 
+			   				$hrefclass = '';	
+			   			 }
+
+				  $link = $item->url;
+				  $title = $item->title;
+				  // item does not have a parent so menu_item_parent equals 0 (false)
+				  if ( !$item->menu_item_parent ) {
+				      // save this id for later comparison with sub-menu items
+				      $parent_id = $item->ID;
+
+				      if ($getCurrenturl == $link) {
+				          $activeClass = 'active';
+				      } else {
+				          $activeClass = '';
+				      }
+				 
+				      echo '<li class="item '.$activeClass. " " .$dropdownclass .'"><a data-toggle="'.$dropdownclass.'" href="'.$link.'" class="title '.$hrefclass.'">'.$title. $icon.'</a>';
+				 
+				  } 
+
+				  if ( $parent_id == $item->menu_item_parent ) {
+
+				      if ( !$submenu ) {
+
+				          	$submenu = true; 
+				  
+				      		echo '<ul class="dropdown-menu" role="menu"><li>';
+
+				      	}
+
+				    echo '<a href="'.$link.'" class="title">'. $title .'</a>';
+
+				       if ( $menuitems[ $count + 1 ]->menu_item_parent != $parent_id && $submenu ) { 
+
+				     		 echo '</ul></li>';
+				   
+				      	$submenu = false;  
+
+				      }
+				  }
+	
+		$count++;}
+
+		echo "</ul>";
+	}
+}
+
+/************ Create Custom Menu html structure ***********/
+
+
+
